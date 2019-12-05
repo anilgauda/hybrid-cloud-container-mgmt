93b0e60744a27befe8bb528b43b9a4266698f54d
Anil Gauda <x18180663@student.ncirl.ie>
2019-12-05 20:02:32 +0000

    Updated Docker Commands.

--- src/main/java/ie/ncirl/container/manager/library/configurevm/constants/VMConstants.java
+++ src/main/java/ie/ncirl/container/manager/library/configurevm/constants/VMConstants.java
@@ -11,7 +11,7 @@ public class VMConstants {
 
     public final static String LINUX_DISTRIBUTION = "cat /etc/os-release | grep ID_LIKE";
     /******** Install and configure Docker Commands for Debian based OS *******************/
-    public final static String DINSTALL_DOCKER_COMMAND = "sudo apt-get install docker -y";
+    public final static String DINSTALL_DOCKER_COMMAND = "sudo snap install docker";
     public final static String START_DOCKER_SERVICE = "sudo service docker start";
 
     /******** Install and configure Docker Commands for Fedora based OS *******************/
@@ -27,23 +27,23 @@ public class VMConstants {
 	public final static String DOCKER_STATUS_COMMAND="sudo service docker status | grep Active";
 	
 	/**** List of docker containers running******/
-	public final static String DOCKER_LIST_CONTAINER="docker container ls -q";
+	public final static String DOCKER_LIST_CONTAINER=" sudo docker container ls -q";
 	
 	/***** List of docker containers with image name *****/
-	public final static String DOCKER_LIST_WITH_NAME_CONTAINER="docker container ls -f ancestor=%s -q";
+	public final static String DOCKER_LIST_WITH_NAME_CONTAINER="sudo docker container ls -f ancestor=%s -q";
 	
 	/******** Get Vm stats*****/
 	public final static String VM_STATS="vmstat";
     public final static String VM_STAT_FREE_MEMORY = "free";
 
     /******************Get Containers Stats ******************/
-	public final static String CONTAINER_STATS="docker stats %s --no-stream";
+	public final static String CONTAINER_STATS=" sudo docker stats %s --no-stream";
 	
 	/***************Docker Container stop******************/
-	public final static String DOCKER_CONTAINER_STOP="docker container stop %s";
+	public final static String DOCKER_CONTAINER_STOP="sudo docker container stop %s";
 	
 	/************** Docker Command to run docker in detacted mode*********/
-	public final static String DOCKER_CONTAINER_START="docker container run -d %s";
+	public final static String DOCKER_CONTAINER_START="sudo docker container run -d %s";
     /******************************Exception Messages*************************************/
     public final static String DOCKER_INSTALL_FAILED_MSG = "Docker Installation Failed";
     public final static String DOCKER_START_FAILED_MSG = "Failed to Start Docker Service";
